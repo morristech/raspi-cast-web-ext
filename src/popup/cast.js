6fa905d034f5b0de1eaccb96da9f6aa6c8a100e1
Giorgian Borca-Tasciuc <giorgianb@openmailbox.org>
2017-09-11 12:32:10 -0400

    Nicer icons

--- src/popup/cast.js
+++ src/popup/cast.js
@@ -1,6 +1,46 @@
+function setTogglePauseIcon(state) {
+	browser.storage.local.set({"state": state});
+	switch (state) {
+		case "play":
+			document.getElementById("togglePause").src = "/icons/play_icon.png";
+			break;
+		case "pause":
+			document.getElementById("togglePause").src = "/icons/pause_icon.png";
+			break;
+	}
+}
+
+function getTogglePauseIcon() {
+	return browser.storage.local.get("state");
+}
+
+function flipTogglePauseIcon() {
+	getTogglePauseIcon().then(
+		(storedData) => {
+			switch (storedData.state) {
+				case "play":
+					setTogglePauseIcon("pause");
+					break;
+				case "pause":
+					setTogglePauseIcon("play");
+					break;
+			}
+		}
+	);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	getTogglePauseIcon().then(
+		(storedData) => {
+			setTogglePauseIcon(storedData.state);
+		}
+	);
+});
+
 document.addEventListener("click", (e) => {
 	switch (e.target.id) {
 		case "cast":
+			setTogglePauseIcon("pause");
 			let currentTab = browser.tabs.query(
 				{
 					active: true,
@@ -35,9 +75,17 @@ document.addEventListener("click", (e) => {
 					if (castID !== null) {
 						let request = new XMLHttpRequest();
 
+						request.onreadystatechange = () => {
+							if (request.readyState = XMLHttpRequest.DONE && request.status == 200) {
+								if (JSON.parse(request.responseText).success)
+									flipTogglePauseIcon();
+							}
+						};
+				
+
 						request.open(
 							"GET",
-							"http://127.0.0.1:8080/?command=togglePause&id=" + escape(castID),
+							"http://127.0.0.1:8080/?command=" + e.target.id + "&id=" + escape(castID),
 							true
 						);
 						request.send();
@@ -45,5 +93,24 @@ document.addEventListener("click", (e) => {
 				}
 			);
 			break;
+		case "skipForward":
+		case "skipBackwards":
+			browser.storage.local.get("castID").then(
+				(storedData) => {
+					const castID = storedData.castID;
+					if (castID !== null) {
+						let request = new XMLHttpRequest();
+
+						request.open(
+							"GET",
+							"http://127.0.0.1:8080/?command=" + e.target.id + "&id=" + escape(castID),
+							true
+						);
+						request.send();
+					}
+				}
+			);
+			break;
+
 	}
 });

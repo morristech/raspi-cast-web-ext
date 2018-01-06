030480b3b8a1c425867b03647072b57a803d86b0
Giorgian Borca-Tasciuc <giorgianb@openmailbox.org>
2018-01-06 18:15:31 -0500

    Added ability to enter IP Address of cast server

--- src/popup/cast.js
+++ src/popup/cast.js
@@ -1,71 +1,101 @@
-const BASE = "http://192.168.0.24:8080/";
-const WEBSOCKET_BASE = "ws://192.168.0.24:1337";
+const COMMAND_PORT = 8080;
+const UPDATE_PORT = 1337;
+
 document.addEventListener("DOMContentLoaded", () => {
+  restoreIPAddress();
   setTogglePauseIcon();
+  document.getElementById("ipAddress").addEventListener("input", storeIPAddress);
   document.getElementById("cast").addEventListener("click", cast);
   document.getElementById("togglePause").addEventListener("click", togglePause);
   document.getElementById("skipForward").addEventListener("click", skipForward);
   document.getElementById("skipBackwards").addEventListener("click", skipBackwards);
   document.getElementById("volumeDown").addEventListener("click", volumeDown);
   document.getElementById("volumeUp").addEventListener("click", volumeUp);
-
-  const connection = new WebSocket(WEBSOCKET_BASE);
-  connection.onmessage = (e) => {
-    const playing = JSON.parse(e.data).isPlaying;
-    document.getElementById("togglePause").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
-  };
+  document.getElementById("toggleSubtitles").addEventListener("click", toggleSubtitles);
 });
 
 
 function setTogglePauseIcon() {
-  sendSimpleCommand("isPlaying", (request) => {
-    if (request.readyState == XMLHttpRequest.DONE && request.status == 200) { 
-      const playing = JSON.parse(request.responseText).isPlaying;
-      document.getElementById("togglePause").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
-    }
+  browser.storage.local.get("ipAddress").then((storedData) => {
+    const ipAddress = storedData.ipAddress;
+    if (ipAddress)
+      sendSimpleCommand("isPlaying", (request) => {
+        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) { 
+          const playing = JSON.parse(request.responseText).isPlaying;
+          document.getElementById("togglePause").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
+        }
+      });
   });
 }
 
 function sendSimpleCommand(command, callback) {
-  let request = new XMLHttpRequest();
+  browser.storage.local.get("ipAddress").then((storedData) => {
+    const ipAddress = storedData.ipAddress;
+    if (ipAddress === "") {
+      alert("Please enter the cast IP address.");
+      return;
+    }
 
-  request.onreadystatechange = () => {
-    if (callback)
-      callback(request);
-  };
+    let request = new XMLHttpRequest();
 
-  request.open("GET",
-    BASE +  command,
-    true
-  );
+    request.onreadystatechange = () => {
+      if (request.readyState = XMLHttpRequest.DONE && request.status >= 400) {
+        clearIPAddress();
+        alert("Incorrect IP Address: " + request.status);
+      } else if (callback)
+        callback(request);
+    };
 
-  request.send();
+    request.open("GET",
+      "http://" + ipAddress + ":" + COMMAND_PORT + "/" + command,
+      true
+    );
+
+    request.send();
+  });
 }
 
 function cast() {
-  let currentTab = browser.tabs.query({
-    active: true,
-    windowId: browser.windows.WINDOW_ID_CURRENT
-  }).then(
-    (tabInfo) => {
-      let tabURL = escape(tabInfo[0].url);
-      let request = new XMLHttpRequest();
+  browser.storage.local.get("ipAddress").then((storedData) => {
+    const ipAddress = storedData.ipAddress;
+    if (ipAddress === "") {
+      alert("Please enter the cast IP address.");
+      return;
+    }
 
-      request.onreadystatechange = () => {
-        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) 
-          setTogglePauseIcon();
-      };
+    let currentTab = browser.tabs.query({
+      active: true,
+      windowId: browser.windows.WINDOW_ID_CURRENT
+    }).then(
+      (tabInfo) => {
+        let tabURL = escape(tabInfo[0].url);
+        let request = new XMLHttpRequest();
 
-      request.open("GET",
-        BASE + "cast?video=" + tabURL,
-        true
-      );
-      request.send();
+        request.onreadystatechange = () => {
+          if (request.readyState = XMLHttpRequest.DONE && request.status >= 400) {
+            clearIPAddress();
+            alert("Incorrect IP Address: " + request.status);
+          } else if (request.readyState == XMLHttpRequest.DONE && request.status == 200) 
+            setTogglePauseIcon();
+        };
 
-    },
-    (error) => {
-      console.log(error);
-    });
+        request.open("GET",
+          "http://" + ipAddress + ":" + COMMAND_PORT + "/cast?video=" + tabURL,
+          true
+        );
+        request.send();
+
+      },
+      (error) => {
+        alert(error);
+      });
+
+    const connection = new WebSocket("ws://" + ipAddress + ":" + UPDATE_PORT);
+    connection.onmessage = (e) => {
+      const playing = JSON.parse(e.data).isPlaying;
+      document.getElementById("togglePause").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
+    };
+  });
 }
 
 function togglePause() {
@@ -86,4 +116,29 @@ function volumeDown() {
 
 function volumeUp() {
   sendSimpleCommand("volumeUp");
+}
+
+function volumeUp() {
+  sendSimpleCommand("volumeUp");
+}
+
+function subtitlesToggle() {
+  sendSimpleCommand("subtitlesToggle");
+}
+
+function storeIPAddress() {
+  browser.storage.local.set({ ipAddress: document.getElementById("ipAddress").value });
+}
+
+function clearIPAddress() {
+  browser.storage.local.set({ ipAddress: "" });
+  document.getElementById("ipAddress").value = "";
+}
+
+function restoreIPAddress() {
+  browser.storage.local.get("ipAddress").then((storedData) => {
+    const ipAddress = storedData.ipAddress;
+    if (ipAddress)
+      document.getElementById("ipAddress").value = ipAddress;
+  });
 }

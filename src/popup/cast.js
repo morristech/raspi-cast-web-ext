216d862711023f08536570a7cb6f08a50e880a47
Giorgian Borca-Tasciuc <giorgianb@openmailbox.org>
2018-03-03 23:39:59 -0500

    play/pause working in new interface and chooses interface base on server version

--- src/popup/cast.js
+++ src/popup/cast.js
@@ -1,59 +1,30 @@
 const COMMAND_PORT = 8080;
 const UPDATE_PORT = 1337;
+const CAST_SERVER_RANGE_SUPPORT_VERSION = "0.11.0";
+const PAUSE_ICON_PATH = "/icons/ic_pause_3x.png";
+const PLAY_ICON_PATH = "/icons/ic_play_arrow_3x.png";
 
 document.addEventListener("DOMContentLoaded", () => {
+  browser.storage.local.get("interface").then((storedData) => {
+    if (storedData.interface == "legacy") {
+      document.getElementById("interface").style.display = "none";
+      document.getElementById("interfaceLegacy").style.display = "block";
+    }
+  });
+
   restoreIPAddress();
-  setTogglePauseIcon();
+  setTogglePlaybackIcon();
+  setTogglePauseIconLegacy();
+  document.getElementById("togglePlaybackStatus").addEventListener("click", togglePlaybackStatus);
   document.getElementById("ipAddress").addEventListener("input", storeIPAddress);
   document.getElementById("cast").addEventListener("click", cast);
-  document.getElementById("togglePause").addEventListener("click", togglePause);
-  document.getElementById("skipForward").addEventListener("click", skipForward);
-  document.getElementById("skipBackwards").addEventListener("click", skipBackwards);
-  document.getElementById("volumeDown").addEventListener("click", volumeDown);
-  document.getElementById("volumeUp").addEventListener("click", volumeUp);
-  document.getElementById("toggleSubtitles").addEventListener("click", toggleSubtitles);
+  document.getElementById("togglePauseLegacy").addEventListener("click", togglePauseLegacy);
+  document.getElementById("skipForwardLegacy").addEventListener("click", skipForwardLegacy);
+  document.getElementById("skipBackwardsLegacy").addEventListener("click", skipBackwardsLegacy);
+  document.getElementById("volumeDownLegacy").addEventListener("click", volumeDownLegacy);
+  document.getElementById("volumeUpLegacy").addEventListener("click", volumeUpLegacy);
+  document.getElementById("toggleSubtitlesLegacy").addEventListener("click", toggleSubtitlesLegacy);
 });
-
-
-function setTogglePauseIcon() {
-  browser.storage.local.get("ipAddress").then((storedData) => {
-    const ipAddress = storedData.ipAddress;
-    if (ipAddress)
-      sendSimpleCommand("isPlaying", (request) => {
-        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) { 
-          const playing = JSON.parse(request.responseText).isPlaying;
-          document.getElementById("togglePause").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
-        }
-      });
-  });
-}
-
-function sendSimpleCommand(command, callback) {
-  browser.storage.local.get("ipAddress").then((storedData) => {
-    const ipAddress = storedData.ipAddress;
-    if (ipAddress === "") {
-      alert("Please enter the cast IP address.");
-      return;
-    }
-
-    let request = new XMLHttpRequest();
-
-    request.onreadystatechange = () => {
-      if (request.readyState = XMLHttpRequest.DONE && request.status >= 400) {
-        clearIPAddress();
-        alert("Incorrect IP Address: " + request.status);
-      } else if (callback)
-        callback(request);
-    };
-
-    request.open("GET",
-      "http://" + ipAddress + ":" + COMMAND_PORT + "/" + command,
-      true
-    );
-
-    request.send();
-  });
-}
 
 function cast() {
   browser.storage.local.get("ipAddress").then((storedData) => {
@@ -72,12 +43,24 @@ function cast() {
         let request = new XMLHttpRequest();
 
         request.onreadystatechange = () => {
-          if (request.readyState = XMLHttpRequest.DONE && request.status >= 400) {
+          if (request.readyState == XMLHttpRequest.DONE && request.status >= 400) {
             clearIPAddress();
             alert("Incorrect IP Address: " + request.status);
-          } else if (request.readyState == XMLHttpRequest.DONE && request.status == 200) 
-            setTogglePauseIcon();
-        };
+          } else if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+            const res = JSON.parse(request.responseText);
+            if ("version" in res && res.version >= CAST_SERVER_RANGE_SUPPORT_VERSION) {
+              document.getElementById("interface").style.display = "block";
+              document.getElementById("interfaceLegacy").style.display = "none";
+              browser.storage.local.set({ "interface" : "new" });
+              setTogglePlaybackStatusIcon();
+            } else {
+              document.getElementById("interface").style.display = "none";
+              document.getElementById("interfaceLegacy").style.display = "block";
+              browser.storage.local.set({ "interface" : "legacy" });
+              setTogglePauseIconLegacy();
+            }
+          } 
+        }
 
         request.open("GET",
           "http://" + ipAddress + ":" + COMMAND_PORT + "/cast?video=" + tabURL,
@@ -93,36 +76,97 @@ function cast() {
     const connection = new WebSocket("ws://" + ipAddress + ":" + UPDATE_PORT);
     connection.onmessage = (e) => {
       const playing = JSON.parse(e.data).isPlaying;
-      document.getElementById("togglePause").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
+      document.getElementById("togglePauseLegacy").src = (playing) ? PAUSE_ICON_PATH : PLAY_ICON_PATH;
+      document.getElementById("togglePlaybackStatus").src = (playing) ? PAUSE_ICON_PATH : PLAY_ICON_PATH;
     };
   });
 }
 
-function togglePause() {
+function setTogglePlaybackIcon() {
+  browser.storage.local.get("ipAddress").then((storedData) => {
+    const ipAddress = storedData.ipAddress;
+    if (ipAddress)
+      sendSimpleCommand("getPlaybackStatus", (request) => {
+        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) { 
+          const playing = JSON.parse(request.responseText).playbackStatus == "Playing";
+          document.getElementById("togglePlaybackStatus").src = (playing) ? PAUSE_ICON_PATH : PLAY_ICON_PATH;
+        }
+      });
+  });
+}
+
+function togglePlaybackStatus() {
+  const playing = document.getElementById("togglePlaybackStatus").src.indexOf(PAUSE_ICON_PATH) >= 0;
+  sendSimpleCommand(playing ? "pause" : "play");
+}
+
+/* Legacy Support Functions */
+function setTogglePauseIconLegacy() {
+  browser.storage.local.get("ipAddress").then((storedData) => {
+    const ipAddress = storedData.ipAddress;
+    if (ipAddress)
+      sendSimpleCommand("isPlaying", (request) => {
+        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) { 
+          const playing = JSON.parse(request.responseText).isPlaying;
+          document.getElementById("togglePauseLegacy").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
+        }
+      });
+  });
+}
+
+function sendSimpleCommand(command, callback) {
+  browser.storage.local.get("ipAddress").then((storedData) => {
+    const ipAddress = storedData.ipAddress;
+    if (ipAddress === "") {
+      alert("Please enter the cast IP address.");
+      return;
+    }
+
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = () => {
+      if (request.readyState == XMLHttpRequest.DONE && request.status >= 400) {
+        clearIPAddress();
+        alert("Incorrect IP Address: " + request.status);
+      } else if (callback)
+        callback(request);
+    };
+
+    request.open("GET",
+      "http://" + ipAddress + ":" + COMMAND_PORT + "/" + command,
+      true
+    );
+
+    request.send();
+  });
+}
+
+
+function togglePauseLegacy() {
   sendSimpleCommand("togglePause");
 }
 
-function skipForward() {
+function skipForwardLegacy() {
   sendSimpleCommand("skipForward");
 }
 
-function skipBackwards() {
+function skipBackwardsLegacy() {
   sendSimpleCommand("skipBackwards");
 }
 
-function volumeDown() {
+function volumeDownLegacy() {
   sendSimpleCommand("volumeDown");
 }
 
-function volumeUp() {
+function volumeUpLegacy() {
   sendSimpleCommand("volumeUp");
 }
 
-function volumeUp() {
+function volumeUpLegacy() {
   sendSimpleCommand("volumeUp");
 }
 
-function toggleSubtitles() {
+function toggleSubtitlesLegacy() {
   sendSimpleCommand("toggleSubtitles");
 }
 
@@ -144,7 +188,7 @@ function restoreIPAddress() {
       const connection = new WebSocket("ws://" + ipAddress + ":" + UPDATE_PORT);
       connection.onmessage = (e) => {
         const playing = JSON.parse(e.data).isPlaying;
-        document.getElementById("togglePause").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
+        document.getElementById("togglePauseLegacy").src = "/icons/" + ((playing) ? "ic_pause_3x.png": "ic_play_arrow_3x.png");
       };
     }
   });

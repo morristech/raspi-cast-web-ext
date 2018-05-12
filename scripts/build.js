00214ea8bedb8ef8b8f3e2e55af1fd5aa74c1b72
Charles Jacquin <charles.jacquin@autistici.org>
2018-05-12 16:20:30 +0200

    chore(webpack): add dev env

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,9 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = process.env.NODE_ENV;
+
+console.log(process.env.NODE_ENV);
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -18,7 +19,6 @@ const path = require('path');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const webpack = require('webpack');
-const config = require('../config/webpack.config.prod');
 const paths = require('../config/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
@@ -33,6 +33,8 @@ const useYarn = fs.existsSync(paths.yarnLockFile);
 // These sizes are pretty large. We'll warn for bundles exceeding them.
 const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
 const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
+
+const config = process.env.NODE_ENV === 'development' ? require('../config/webpack.config.dev') : require('../config/webpack.config.prod');
 
 // First, read the current file sizes in build directory.
 // This lets us display how much they changed later.
